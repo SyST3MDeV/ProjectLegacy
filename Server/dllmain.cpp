9a9a9af5ea5fe36789eeea7f7c95e760f718df40
gwog :3 <the@realsystem.dev>
2024-08-29 09:49:57 -0400

    nonfunctional game start logic, proc in game thread needs a refactor

--- Server/dllmain.cpp
+++ Server/dllmain.cpp
@@ -10,11 +10,47 @@
 
 using namespace CG;
 
+namespace SDKUtils {
+    template <typename T>
+    std::vector<T*> GetAllObjectsOfType() {
+        return UObject::FindObjects<T>();
+    }
+
+    template <typename T>
+    void ListAllObjectsOfType() {
+        for (T* obj : GetAllObjectsOfType<T>()) {
+            UObject* castObj = reinterpret_cast<UObject*>(obj);
+
+            std::cout << castObj->GetFullName() << std::endl;
+        }
+    }
+}
+
 namespace Globals {
     uintptr_t ModuleBase = 0;
 
     UWorld* GetGWorld() {
         return *UWorld::GWorld;
+    }
+
+    UGameplayStatics* GetGameplayStatics() {
+        static UGameplayStatics* gameplayStatics = nullptr;
+
+        if (!gameplayStatics) {
+            gameplayStatics = UObject::FindObject<UGameplayStatics>();
+        }
+
+        return gameplayStatics;
+    }
+
+    UKismetStringLibrary* GetKismetStringLibrary() {
+        static UKismetStringLibrary* kismetStringLibrary = nullptr;
+
+        if (!kismetStringLibrary) {
+            kismetStringLibrary = UObject::FindObject<UKismetStringLibrary>();
+        }
+
+        return kismetStringLibrary;
     }
 
     template <typename T>
@@ -38,25 +74,7 @@ namespace Globals {
     }
 }
 
-namespace Hooking {
-    void* origProcessEvent = nullptr;
-
-    void* ProcessEventHook(UObject* object, UFunction* function, void* params) {
-        return reinterpret_cast<void*(__thiscall*)(UObject*, UFunction*, void*)>(object, function, params);
-    }
-
-    void InitHooking() {
-        MH_Initialize();
-
-        void* ProcessEventHookLocal = (void*)(Globals::ModuleBase + 0xFB3420);
-
-        MH_CreateHook(ProcessEventHookLocal, reinterpret_cast<void*>(ProcessEventHook), &origProcessEvent);
-
-        //MH_EnableHook(ProcessEventHookLocal);
-    }
-}
-
-namespace GameLogic {
+namespace EngineLogic {
     void EnableGameConsole() {
         CG::UKismetStringLibrary* stringLibrary = UObject::FindObjects<CG::UKismetStringLibrary>().back();
         CG::UEngine* engine = UObject::FindObjects<CG::UEngine>().back();
@@ -96,6 +114,88 @@ namespace GameLogic {
         systemLibrary->STATIC_OpenLevel(Globals::GetGWorld(), kismetStringLibrary->STATIC_Conv_StringToName(map), true, options);
     }
 
+    void* Malloc(__int64 size, unsigned int alignment) {
+        return reinterpret_cast<void* (__thiscall*)(__int64 size, unsigned int alignment)>(Globals::ModuleBase + 0xDFB9F0)(size, alignment);
+    }
+}
+
+namespace GameLogic {
+    void SetupTeams() {
+        AOrionGameState_MOBA* gameState = Globals::GetGameState<AOrionGameState_MOBA>();
+
+        gameState->Teams._data = reinterpret_cast<AOrionTeamInfo**>(EngineLogic::Malloc(sizeof(AOrionTeamInfo*) * 3, 0));
+        gameState->Teams._count = 4;
+        gameState->Teams._max = 4;
+
+        AOrionTeamInfo* redTeamInfo = reinterpret_cast<AOrionTeamInfo*>(Globals::GetGameplayStatics()->STATIC_BeginSpawningActorFromClass(Globals::GetGWorld(), AOrionTeamInfo::StaticClass(), FTransform(), true, gameState));
+
+        redTeamInfo->TeamIndex = EOrionTeam::TeamRed;
+        redTeamInfo->bPlayerTeam = true;
+        redTeamInfo->MaxPlayers = 5;
+        redTeamInfo->TeamName = Globals::GetKismetStringLibrary()->STATIC_Conv_StringToName(L"RedTeam");
+
+        redTeamInfo->TeamMembers._data = reinterpret_cast<AController**>(EngineLogic::Malloc(sizeof(AController*) * 5, 0));
+        redTeamInfo->TeamMembers._count = 0;
+        redTeamInfo->TeamMembers._max = 5;
+
+        AOrionTeamInfo* blueTeamInfo = reinterpret_cast<AOrionTeamInfo*>(Globals::GetGameplayStatics()->STATIC_BeginSpawningActorFromClass(Globals::GetGWorld(), AOrionTeamInfo::StaticClass(), FTransform(), true, gameState));
+
+        blueTeamInfo->TeamIndex = EOrionTeam::TeamBlue;
+        blueTeamInfo->bPlayerTeam = true;
+        blueTeamInfo->MaxPlayers = 5;
+        blueTeamInfo->TeamName = Globals::GetKismetStringLibrary()->STATIC_Conv_StringToName(L"BlueTeam");
+
+        blueTeamInfo->TeamMembers._data = reinterpret_cast<AController**>(EngineLogic::Malloc(sizeof(AController*) * 5, 0));
+        blueTeamInfo->TeamMembers._count = 0;
+        blueTeamInfo->TeamMembers._max = 5;
+
+        AOrionTeamInfo* teamCreepInfo = reinterpret_cast<AOrionTeamInfo*>(Globals::GetGameplayStatics()->STATIC_BeginSpawningActorFromClass(Globals::GetGWorld(), AOrionTeamInfo::StaticClass(), FTransform(), true, gameState));
+
+        teamCreepInfo->TeamIndex = EOrionTeam::TeamCreep;
+        teamCreepInfo->bPlayerTeam = true;
+        teamCreepInfo->MaxPlayers = 0;
+        teamCreepInfo->TeamName = Globals::GetKismetStringLibrary()->STATIC_Conv_StringToName(L"RedTeam");
+
+        gameState->Teams[0] = redTeamInfo;
+        gameState->Teams[1] = blueTeamInfo;
+        gameState->Teams[3] = teamCreepInfo;
+    }
+
+    bool AddControllerToTeam(AOrionPlayerController_Game* controller, EOrionTeam team) {
+        AOrionGameState_MOBA* gameState = Globals::GetGameState<AOrionGameState_MOBA>();
+
+        for (int i = 0; i < gameState->Teams.Count(); i++) {
+            AOrionTeamInfo* teamInfo = gameState->Teams[i];
+
+            if (teamInfo->TeamIndex == team) {
+                if (teamInfo->TeamMembers.Count() < 5) {
+                    teamInfo->TeamMembers[teamInfo->TeamMembers.Count()] = controller;
+                    teamInfo->TeamMembers._count = teamInfo->TeamMembers._count + 1;
+                    controller->ServerChangeTeam(team);
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    void StartMatch() {
+        Globals::GetLocalPlayerController<AOrionPlayerController_Game>()->ServerForceStartGame();
+    }
+
+    void SetControllerHeroData(AOrionPlayerController_Game* controller, UOrionHeroData* heroData, UOrionSkinItemDefinition* skin) {
+        FOrionHeroDataSpec spec = FOrionHeroDataSpec();
+
+        spec.HeroData = heroData;
+        spec.Skin = skin;
+
+        reinterpret_cast<AOrionPlayerState_Game*>(controller->PlayerState)->ServerSetPlayerHeroDataSpec(spec);
+
+        controller->HeroData = heroData;
+        controller->HeroName = L"Kwang";
+    }
+
     void SetUIState(EOrionUIState state) {
         reinterpret_cast<void(__thiscall*)(UOrionUIManagerWidget*, EOrionUIState)>(Globals::ModuleBase + 0x7DDBC0)(reinterpret_cast<UOrionGameInstance*>(Globals::GetGWorld()->OwningGameInstance)->UIManager, state);
     }
@@ -107,16 +207,59 @@ void InitConsole() {
     freopen_s(&f, "CONOUT$", "w", stdout);
 }
 
+static bool procInGameThread = false;
+
 void OnGameInit() {
     std::cout << "Enabling game console..." << std::endl;
-    GameLogic::EnableGameConsole();
+    EngineLogic::EnableGameConsole();
 
     std::cout << "Loading map..." << std::endl;
-    GameLogic::LoadMap(L"Origin_1v1", L"");
+    EngineLogic::LoadMap(L"Agora_P", L"");
+
+    Sleep(20 * 1000);
+
+    procInGameThread = true;
 }
 
 void MainLoop() {
 
+}
+
+namespace Hooking {
+    void* origProcessEvent = nullptr;
+
+    void* ProcessEventHook(UObject* object, UFunction* function, void* params) {
+        if (procInGameThread) {
+            procInGameThread = false;
+
+            std::cout << "Setting UI state..." << std::endl;
+            GameLogic::SetUIState(EOrionUIState::Match);
+
+            std::cout << "Setting up teams..." << std::endl;
+            GameLogic::SetupTeams();
+
+            std::cout << "Adding local player to team blue..." << std::endl;
+            GameLogic::AddControllerToTeam(Globals::GetLocalPlayerController<AOrionPlayerController_Game>(), EOrionTeam::TeamBlue);
+
+            std::cout << "Setting local player hero data..." << std::endl;
+            GameLogic::SetControllerHeroData(Globals::GetLocalPlayerController<AOrionPlayerController_Game>(), UObject::FindObject<UOrionHeroData>("OrionHeroData HeroData_ArcBlade.HeroData_ArcBlade"), UObject::FindObject<UOrionSkinItemDefinition>("OrionSkinItemDefinition MasterSkin_Arcblade.MasterSkin_Arcblade"));
+
+            std::cout << "Starting the match..." << std::endl;
+            GameLogic::StartMatch();
+        }
+
+        return reinterpret_cast<void* (__thiscall*)(UObject*, UFunction*, void*)>(origProcessEvent)(object, function, params);
+    }
+
+    void InitHooking() {
+        MH_Initialize();
+
+        void* ProcessEventHookLocal = (void*)(Globals::ModuleBase + 0xFB3420);
+
+        MH_CreateHook(ProcessEventHookLocal, reinterpret_cast<void*>(ProcessEventHook), &origProcessEvent);
+
+        MH_EnableHook(ProcessEventHookLocal);
+    }
 }
 
 void Main() {
