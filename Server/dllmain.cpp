9dcf9cfe40d21970d93f1a525ebc2b52a799b556
gwog :3 <the@realsystem.dev>
2024-08-29 17:57:48 -0400

    Mostly functional player spawning

--- Server/dllmain.cpp
+++ Server/dllmain.cpp
@@ -149,6 +149,13 @@ namespace GameLogic {
         blueTeamInfo->TeamMembers._count = 0;
         blueTeamInfo->TeamMembers._max = 5;
 
+        AOrionTeamInfo* noTeamInfo = reinterpret_cast<AOrionTeamInfo*>(Globals::GetGameplayStatics()->STATIC_BeginSpawningActorFromClass(Globals::GetGWorld(), AOrionTeamInfo::StaticClass(), FTransform(), true, gameState));
+
+        noTeamInfo->TeamIndex = EOrionTeam::NoTeam;
+        noTeamInfo->bPlayerTeam = true;
+        noTeamInfo->MaxPlayers = 0;
+        noTeamInfo->TeamName = Globals::GetKismetStringLibrary()->STATIC_Conv_StringToName(L"RedTeam");
+
         AOrionTeamInfo* teamCreepInfo = reinterpret_cast<AOrionTeamInfo*>(Globals::GetGameplayStatics()->STATIC_BeginSpawningActorFromClass(Globals::GetGWorld(), AOrionTeamInfo::StaticClass(), FTransform(), true, gameState));
 
         teamCreepInfo->TeamIndex = EOrionTeam::TeamCreep;
@@ -158,6 +165,7 @@ namespace GameLogic {
 
         gameState->Teams[0] = redTeamInfo;
         gameState->Teams[1] = blueTeamInfo;
+        gameState->Teams[2] = noTeamInfo;
         gameState->Teams[3] = teamCreepInfo;
     }
 
@@ -172,6 +180,8 @@ namespace GameLogic {
                     teamInfo->TeamMembers[teamInfo->TeamMembers.Count()] = controller;
                     teamInfo->TeamMembers._count = teamInfo->TeamMembers._count + 1;
                     controller->ServerChangeTeam(team);
+                    reinterpret_cast<AOrionPlayerState_Game*>(controller->PlayerState)->TeamInfo = teamInfo;
+                    reinterpret_cast<AOrionPlayerState_Game*>(controller->PlayerState)->bReadyToStartMatch = true;
                     return true;
                 }
             }
@@ -194,61 +204,56 @@ namespace GameLogic {
 
         controller->HeroData = heroData;
         controller->HeroName = L"Kwang";
+
+        reinterpret_cast<AOrionPlayerState_Game*>(controller->PlayerState)->HeroDataSpec = spec;
     }
 
     void SetUIState(EOrionUIState state) {
         reinterpret_cast<void(__thiscall*)(UOrionUIManagerWidget*, EOrionUIState)>(Globals::ModuleBase + 0x7DDBC0)(reinterpret_cast<UOrionGameInstance*>(Globals::GetGWorld()->OwningGameInstance)->UIManager, state);
     }
-}
 
-void InitConsole() {
-    AllocConsole();
-    FILE* f = new FILE();
-    freopen_s(&f, "CONOUT$", "w", stdout);
-}
+    void RestartPlayer(AOrionPlayerController_Game* pc) {
+        pc->ServerRestartPlayer();
+    }
 
-static bool procInGameThread = false;
-
-void OnGameInit() {
-    std::cout << "Enabling game console..." << std::endl;
-    EngineLogic::EnableGameConsole();
-
-    std::cout << "Loading map..." << std::endl;
-    EngineLogic::LoadMap(L"Agora_P", L"");
-
-    Sleep(20 * 1000);
-
-    procInGameThread = true;
-}
-
-void MainLoop() {
-
+    void HideLoadingScreen() {
+        EngineLogic::ExecuteConsoleCommand(L"hideloadingscreen");
+    }
 }
 
 namespace Hooking {
+    bool procingCurrentFuncPtrs = false;
+
+    std::vector<void*> FuncPtrsToProcInGameThread = std::vector<void*>();
+
+    void ProcInGameThread(void* ptr) {
+        FuncPtrsToProcInGameThread.push_back(ptr);
+    }
+
     void* origProcessEvent = nullptr;
 
     void* ProcessEventHook(UObject* object, UFunction* function, void* params) {
-        if (procInGameThread) {
-            procInGameThread = false;
+        if (!procingCurrentFuncPtrs && FuncPtrsToProcInGameThread.size() > 0) {
+            procingCurrentFuncPtrs = true;
 
-            std::cout << "Setting UI state..." << std::endl;
-            GameLogic::SetUIState(EOrionUIState::Match);
+            while (FuncPtrsToProcInGameThread.size() > 0) {
+                void* funcPtr = FuncPtrsToProcInGameThread.back();
 
-            std::cout << "Setting up teams..." << std::endl;
-            GameLogic::SetupTeams();
+                reinterpret_cast<void(*)()>(funcPtr)();
 
-            std::cout << "Adding local player to team blue..." << std::endl;
-            GameLogic::AddControllerToTeam(Globals::GetLocalPlayerController<AOrionPlayerController_Game>(), EOrionTeam::TeamBlue);
-
-            std::cout << "Setting local player hero data..." << std::endl;
-            GameLogic::SetControllerHeroData(Globals::GetLocalPlayerController<AOrionPlayerController_Game>(), UObject::FindObject<UOrionHeroData>("OrionHeroData HeroData_ArcBlade.HeroData_ArcBlade"), UObject::FindObject<UOrionSkinItemDefinition>("OrionSkinItemDefinition MasterSkin_Arcblade.MasterSkin_Arcblade"));
-
-            std::cout << "Starting the match..." << std::endl;
-            GameLogic::StartMatch();
+                FuncPtrsToProcInGameThread.pop_back();
+            }
         }
 
         return reinterpret_cast<void* (__thiscall*)(UObject*, UFunction*, void*)>(origProcessEvent)(object, function, params);
+    }
+
+    void* aactorGetNetMode = nullptr;
+    void* uworldGetNetMode = nullptr;
+    void* uengineGetNetMode = nullptr;
+
+    __int64 GetNetModeHook() {
+        return 2;
     }
 
     void InitHooking() {
@@ -259,7 +264,67 @@ namespace Hooking {
         MH_CreateHook(ProcessEventHookLocal, reinterpret_cast<void*>(ProcessEventHook), &origProcessEvent);
 
         MH_EnableHook(ProcessEventHookLocal);
+
+        void* aactorGetNetModeHook = (void*)(Globals::ModuleBase + 0x1B8B180);
+
+        MH_CreateHook(aactorGetNetModeHook, reinterpret_cast<void*>(GetNetModeHook), &aactorGetNetMode);
+
+        MH_EnableHook(aactorGetNetModeHook);
+
+        void* uworldGetNetModeHook = (void*)(Globals::ModuleBase + 0x2313470);
+
+        MH_CreateHook(uworldGetNetModeHook, reinterpret_cast<void*>(GetNetModeHook), &uworldGetNetMode);
+
+        MH_EnableHook(uworldGetNetModeHook);
+
+        void* uengineGetNetModeHook = (void*)(Globals::ModuleBase + 0x22A4920);
+
+        MH_CreateHook(uengineGetNetModeHook, reinterpret_cast<void*>(GetNetModeHook), &uengineGetNetMode);
+
+        MH_EnableHook(uengineGetNetModeHook);
     }
+}
+
+void InitConsole() {
+    AllocConsole();
+    FILE* f = new FILE();
+    freopen_s(&f, "CONOUT$", "w", stdout);
+}
+
+void OnMatchInit() {
+    std::cout << "Setting UI state..." << std::endl;
+    GameLogic::SetUIState(EOrionUIState::Match);
+
+    std::cout << "Setting up teams..." << std::endl;
+    GameLogic::SetupTeams();
+
+    std::cout << "Adding local player to team blue..." << std::endl;
+    GameLogic::AddControllerToTeam(Globals::GetLocalPlayerController<AOrionPlayerController_Game>(), EOrionTeam::TeamBlue);
+
+    std::cout << "Setting local player hero data..." << std::endl;
+    GameLogic::SetControllerHeroData(Globals::GetLocalPlayerController<AOrionPlayerController_Game>(), UObject::FindObject<UOrionHeroData>("OrionHeroData HeroData_ArcBlade.HeroData_ArcBlade"), UObject::FindObject<UOrionSkinItemDefinition>("OrionSkinItemDefinition MasterSkin_Arcblade.MasterSkin_Arcblade"));
+
+    std::cout << "Starting the match..." << std::endl;
+    GameLogic::StartMatch();
+
+    std::cout << "Hiding loading screen..." << std::endl;
+    GameLogic::HideLoadingScreen();
+}
+
+void OnGameInit() {
+    std::cout << "Enabling game console..." << std::endl;
+    EngineLogic::EnableGameConsole();
+
+    std::cout << "Loading map..." << std::endl;
+    EngineLogic::LoadMap(L"Agora_P", L"");
+
+    Sleep(20 * 1000);
+
+    Hooking::ProcInGameThread(OnMatchInit);
+}
+
+void MainLoop() {
+
 }
 
 void Main() {
