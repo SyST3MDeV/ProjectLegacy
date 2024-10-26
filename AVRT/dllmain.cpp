d8d0025d6b584d9fed8c21454cb2dfc2113df9c3
gwog :3 <the@realsystem.dev>
2024-10-25 21:12:22 -0400

    update to offsets namespace yada yada but for the dll shim this time

--- AVRT/dllmain.cpp
+++ AVRT/dllmain.cpp
@@ -12,6 +12,12 @@
 
 #define IS_SERVER true
 
+namespace Offsets {
+    //Hooking Offsets
+    static const uintptr_t INIT_NORMAL_RHI = 0x13BF510;
+    static const uintptr_t INIT_NULL_RHI = 0x13B7EC0;
+}
+
 extern "C" LONG __stdcall CallNtPowerInformationFake(POWER_INFORMATION_LEVEL InformationLevel, PVOID InputBuffer, ULONG InputBufferLength, PVOID OutputBuffer, ULONG OutputBufferLength) {
     return CallNtPowerInformation(InformationLevel, InputBuffer, InputBufferLength, OutputBuffer, OutputBufferLength);
 }
@@ -22,14 +28,14 @@ extern "C" POWER_PLATFORM_ROLE PowerDeterminePlatformRoleFake() {
 
 void* origInitRHI = nullptr;
 void InitRHIHook(__int64 a1) {
-    reinterpret_cast<void(*)()>((uintptr_t)GetModuleHandleA("OrionClient-Win64-Shipping.exe") + 0x13B7EC0)();
+    reinterpret_cast<void(*)()>((uintptr_t)GetModuleHandleA("OrionClient-Win64-Shipping.exe") + Offsets::INIT_NULL_RHI)();
 }
 
 void LoadLibraryThread() {
 #if IS_SERVER
     MH_Initialize();
 
-    void* initRHI = (void*)((uintptr_t)GetModuleHandleA("OrionClient-Win64-Shipping.exe") + 0x13BF510);
+    void* initRHI = (void*)((uintptr_t)GetModuleHandleA("OrionClient-Win64-Shipping.exe") + Offsets::INIT_NORMAL_RHI);
 
     MH_CreateHook(initRHI, reinterpret_cast<void*>(InitRHIHook), &origInitRHI);
 
